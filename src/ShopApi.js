901904f86d0d51ddfe46418a43f36fd9e42ac30a
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-09-15 03:45:47 -0400

    Categorys finished

--- src/ShopApi.js
+++ src/ShopApi.js
@@ -1,25 +1,21 @@
 import axios from 'axios';
-import { Setshop } from './redux/reducers/shop';
-import { Setproducts } from './redux/reducers/products';
 
 const baseURL = 'https://fakestoreapi.com/';
 
-export const loadCategorys = async (dispatch) => {
-  const categorys = await axios
-    .get(`${baseURL}products/categories`)
+export const loadCategorys = async () => {
+  const categorys = await axios.get(`${baseURL}products/categories`)
     .then((res) => {
       const result = res.data;
       return result;
     });
-  dispatch(Setshop(categorys));
-};
+    return categorys
+  };
 
-export const LoadproductsByCategory = async (dispatch, string) => {
-  const category = await axios
-    .get(`${baseURL}products/category/${string}`)
+export const LoadproductsByCategory = async () => {
+  const category = await axios.get(`${baseURL}products/category/${string}`)
     .then((res) => {
       const result = res.data;
       return result;
     });
-  dispatch(Setproducts({ value: category, key: string}));
+    return category
 };
--- src/components/categorys/categorys.js
+++ src/components/categorys/categorys.js
@@ -1,19 +1,22 @@
-import { loadCategorys } from '../../ShopApi';
 import { useSelector, useDispatch } from 'react-redux';
 import React, { useEffect } from 'react';
-
+import { loadCategorys } from '../../ShopApi';
+import { Setshop } from '../../redux/reducers/shop';
 
 const Categorys = () => {
-    const categorys = useSelector(state => state.shop);
-    const dispatch = useDispatch();
-    if (categorys.length === 0) {
-        loadCategorys(dispatch);
-      }
-      console.log(categorys)
-
+  const categorys = useSelector((state) => state.shop);
+  const dispatch = useDispatch();
+  useEffect(async () => {
+    const result = await loadCategorys();
+    dispatch(Setshop(result));
+  }, []);
   return (
-    <div className="Categorys">
-    </div>
+    <div className="Categorys" >{categorys.map((cat) => {
+      return (
+        <div key={cat}>{cat}
+        </div>
+      );
+    })} </div>
   );
 };
 
